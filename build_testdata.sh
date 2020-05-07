d3a6224d72f8157ebacbbb266b5c54b3aaa25f13
Xuejie Xiao <xxuejie@gmail.com>
2020-05-07 03:04:13 +0000

    chore: Move Rust crate to a separate organized folder

--- build_testdata.sh
+++ build_testdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
 
-riscv64-unknown-elf-gcc -o native/testdata/always_success native/testdata/always_success.S -nostdlib -nostartfiles
-make build-smt VM_FILES=c/vms/dummy/dummy_vm.c OUTPUT=native/testdata/dummy_smt_validator
-make build-smt VM_FILES=c/vms/dummy/dummy_vm.c OUTPUT=native/testdata/dummy_smt_generator CUSTOM_CFLAGS=-DBUILD_GENERATOR
+riscv64-unknown-elf-gcc -o crates/layer/testdata/always_success crates/layer/testdata/always_success.S -nostdlib -nostartfiles
+make build-smt VM_FILES=c/vms/dummy/dummy_vm.c OUTPUT=crates/layer/testdata/dummy_smt_validator
+make build-smt VM_FILES=c/vms/dummy/dummy_vm.c OUTPUT=crates/layer/testdata/dummy_smt_generator CUSTOM_CFLAGS=-DBUILD_GENERATOR
